7802f153b45559bf35a775e90e31231ace25c426
Your Name <you@example.com>
2022-08-16 22:59:24 -0500

    Agregado proyecto Python

---
+++ app-compose-python/PY_Examples/test_app.py
@@ -0,0 +1,15 @@
+
+def test_suma():
+    assert (2+2) == 4
+
+def test_suma2():
+    assert (2+2) == 4
+
+def test_suma3():
+    assert (2+2) == 4
+
+def test_suma4():
+    assert (2+2) == 4
+
+def test_suma5():
+    assert (2+2) == 4
