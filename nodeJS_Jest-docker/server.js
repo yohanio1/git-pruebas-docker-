bb1945f78bfe7d0ef9cfd595629355ac03ffff7d
Frank <juane.acevedoc@gmail.com>
2022-08-20 19:38:01 -0500

    Añadido proyectos Flask-docker, mysqldb-flask, nodeJs_Jest, python-app-virtual

---
+++ nodeJS_Jest-docker/server.js
@@ -0,0 +1,9 @@
+
+
+const ronin = require('ronin-server')
+const mocks = require('ronin-mocks')
+
+const server = ronin.server()
+
+server.use('/', mocks.server(server.Router(), false, true))
+server.start()
