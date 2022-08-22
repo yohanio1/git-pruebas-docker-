79db7cc405d1280c31a8e28f6df46581c66d373c
Juan <juane.acevedoc@hotmail.com>
2022-08-21 21:27:42 -0500

    nuevas pruebas selenium

--- selenium/selenium-app.py
+++ selenium/selenium-app.py
@@ -2,13 +2,18 @@ from lib2to3.pgen2 import driver
 import time
 import unittest
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome("./driver/chromedriver")
+        self.driver = webdriver.Chrome(executable_path="C:\driver\chromedriver")
+    
+    def tearDown(self):
+        self.driver.close()
+
 
     # def test_search_in_python_org(self):
     #     driver = self.driver
@@ -43,20 +48,38 @@ class PythonOrgSearch(unittest.TestCase):
     #     elem.send_keys(Keys.RETURN)
     #     time.sleep(3)
 
-    def test_back_forward(self):   
+    # def test_back_forward(self):   
+
+    #     driver = self.driver
+    #     driver.get("http://www.google.com.co")
+    #     time.sleep(3)
+    #     driver.get("https://www.youtube.com")
+    #     time.sleep(3)
+    #     driver.back()
+
+    # def test_toggle_click(self):   
+
+    #     driver = self.driver
+    #     driver.get("https://www.w3schools.com/howto/howto_css_switch.asp")
+    #     select = driver.find_element(By.XPATH,"//*[@id='main']/label[3]/div")
+    #     select.click()
+    #     time.sleep(3)
+    #     select.click()
+    #     time.sleep(3)
+        
+
+    def test_select_list(self):   
 
         driver = self.driver
-        driver.get("http://www.google.com.co")
-        time.sleep(3)
-        driver.get("https://www.youtube.com")
-        time.sleep(3)
-        driver.back()
-        time.sleep(3)
+        driver.get("https://www.w3schools.com/howto/howto_custom_select.asp")
+        select = driver.find_element(By.XPATH,"//*[@id='main']/div[3]/div[1]/select")
+        options = select.find_elements(By.TAG_NAME,"option")
 
+        for option in options:
+            print(option.get_attribute("value"))
 
-
-    def tearDown(self):
-        self.driver.close()
-
+        seleccionar = Select(driver.find_element(By.XPATH,"//*[@id='main']/div[3]/div[1]/select"))
+        seleccionar.select_by_value("2")
+        
 if __name__ == "__main__":
     unittest.main()
